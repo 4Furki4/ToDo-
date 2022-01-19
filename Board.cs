e830825de3554ea23b78a6c7e240aef64b4b052b
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-01-20 00:04:01 +0300

    Kart taşıma metodu board class'ına eklendi.

--- Board.cs
+++ Board.cs
@@ -64,14 +64,186 @@ namespace ToDo
         {
             Console.WriteLine("Hatali giriş yaptınız");
         }
-        void KartSilme()
+        void KartSilme(List<Kart> TODO_LIST, List<Kart> INRPOGRESS_LIST, List<Kart> DONE_LIST)
         {
-            
+            Console.WriteLine("Lütfen silmek istediğiniz kartın başlığını giriniz:");
+            string input=Console.ReadLine();
+            int sayac=0;
+            foreach (var item in TODO_LIST)
+            {
+                if (item.Baslık==input)
+                {
+                    TODO_LIST.Remove(item);
+                    sayac++;
+                }
+            }
+            foreach (var item in INRPOGRESS_LIST)
+            {
+                if (item.Baslık==input)
+                {
+                    TODO_LIST.Remove(item);
+                    sayac++;
+                }
+            }
+            foreach (var item in DONE_LIST)
+            {
+                if (item.Baslık==input)
+                {
+                    TODO_LIST.Remove(item);
+                    sayac++;
+                }
+            }
+            if (sayac==0)
+            {
+                Console.WriteLine("Aradığınız krtiterlere uygun kart board'da bulunamadı. Lütfen bir seçim yapınız.");
+                Console.WriteLine(" * Silmeyi sonlandırmak için : (1)\n* Yeniden denemek için : (2)");
+                int islem= int.Parse(Console.ReadLine());
+                if (islem==1)
+                {
+                   Console.WriteLine("Kart silme işlemi başarıyla sonlandırıldı.");
+                }else if (islem==2)
+                {
+                    KartSilme(TODO_LIST, INRPOGRESS_LIST, DONE_LIST);
+                }else 
+                {
+                    Console.WriteLine("Geçersiz bir değer girdiniz.");
+                }
+            }
+
         }
             
+        void KartTasima(List<Kart> TODO_LIST, List<Kart> INRPOGRESS_LIST, List<Kart> DONE_LIST)
+        {
+            int tasima=0;
+            Console.WriteLine("Lütfen taşımak istediğiniz kartın başlığını giriniz:");
+            string input= Console.ReadLine();
+            foreach (var item in TODO_LIST)
+            {
+                if (item.Baslık==input)
+                {
+                    Console.WriteLine("Bulunan Kart bilgileri \n*******************************************");
+                    Console.WriteLine($"Başlık               :{item.Baslık}");
+                    Console.WriteLine($"İçerik               :{item.Icerik}");
+                    Console.WriteLine($"Atanan Kişi          :{item.AtananKisi}");
+                    Console.WriteLine($"Büyüklük             :{(Kart.Buyukluk)item.buyukluk}");
+                    Console.WriteLine($"Line                 :TODO LIST");
+                    tasima++;
+                    Console.WriteLine("Lütfen taşımak istediğiniz Line'ı seçiniz:\n(1) TODO\n(2) IN PROGRESS\n(3) DONE");
+                    int tasinacakYer=int.Parse(Console.ReadLine());
+                    
+                    if (tasinacakYer==1)
+                    {
+                        // AYNI YERE KART TAŞINMAYACAĞI İÇİN KODU BOŞ BIRAKTIM.
+                    }
+                    else if (tasinacakYer==2)
+                    {
+                        TODO_List.Remove(item);
+                        INRPOGRESS_LIST.Add(item);
+                    }
+                    else if (tasinacakYer==3)
+                    {
+                        TODO_List.Remove(item);
+                        DONE_LIST.Add(item);
 
+                    }
+                    else
+                    {
+                        Console.WriteLine("Hatalı giriş yaptınız!");
+                    }
+                }
+            }
+            if (tasima==0)
+            {
+                foreach (var item in INRPOGRESS_LIST)
+                {
+                    if (item.Baslık==input)
+                    {
+                    Console.WriteLine("Bulunan Kart bilgileri \n*******************************************");
+                    Console.WriteLine($"Başlık               :{item.Baslık}");
+                    Console.WriteLine($"İçerik               :{item.Icerik}");
+                    Console.WriteLine($"Atanan Kişi          :{item.AtananKisi}");
+                    Console.WriteLine($"Büyüklük             :{(Kart.Buyukluk)item.buyukluk}");
+                    Console.WriteLine($"Line                 :IN RPOGRESS LIST");
+                    tasima++;
+                    Console.WriteLine("Lütfen taşımak istediğiniz Line'ı seçiniz:\n(1) TODO\n(2) IN PROGRESS\n(3) DONE");
+                    int tasinacakYer=int.Parse(Console.ReadLine());
+                    
+                        if (tasinacakYer==1)
+                        {
+                           INPROGRESS.Remove(item);
+                           TODO_List.Add(item);
+                        }
+                        else if (tasinacakYer==2)
+                        {
+                            // AYNI YERE KART TAŞINMAYACAĞI İÇİN KODU BOŞ BIRAKTIM.
+                        }
+                        else if (tasinacakYer==3)
+                        {
+                            INRPOGRESS_LIST.Remove(item);
+                            DONE_LIST.Add(item);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Hatalı giriş yaptınız!");
+                        }
+                    }
+                }
+            }
+            if (tasima==0)
+                {
+               foreach (var item in DONE_LIST)
+                {
+                    if (item.Baslık==input)
+                    {
+                        tasima++;
+                        Console.WriteLine("Bulunan Kart bilgileri \n*******************************************");
+                        Console.WriteLine($"Başlık               :{item.Baslık}");
+                        Console.WriteLine($"İçerik               :{item.Icerik}");
+                        Console.WriteLine($"Atanan Kişi          :{item.AtananKisi}");
+                        Console.WriteLine($"Büyüklük             :{(Kart.Buyukluk)item.buyukluk}");
+                        Console.WriteLine($"Line                 :DONE LIST");
+                        Console.WriteLine("Lütfen taşımak istediğiniz Line'ı seçiniz:\n(1) TODO\n(2) IN PROGRESS\n(3) DONE");
+                        int tasinacakYer=int.Parse(Console.ReadLine());
+
+                            if (tasinacakYer==1)
+                            {
+                                DONE_LIST.Remove(item);
+                                TODO_List.Add(item);
+                            }else if (tasinacakYer==2)
+                            {
+                                DONE_LIST.Remove(item);
+                                INRPOGRESS_LIST.Add(item);
+                            }else if (tasinacakYer==3)
+                            {
+                                // AYNI YERE KART TAŞINMAYACAĞI İÇİN KODU BOŞ BIRAKTIM.
+                            }else
+                            {
+                                Console.WriteLine("Hatalı giriş yaptınız!");
+                            }
+
+                    }
+                } 
+            }
+            else if (tasima==0) //taşıma hala 0 ise, kart bulunamamış demektir.
+            {
+                Console.WriteLine("Aradığınız krtiterlere uygun kart board'da bulunamadı. Lütfen bir seçim yapınız");
+                Console.WriteLine("* İşlemi sonlandırmak için : (1)\n* Yeniden denemek için : (2)");
+                int islem= int.Parse(Console.ReadLine());
+                    if (islem==1)
+                    {
+                    Console.WriteLine("Kart taşıma işlemi başarıyla sonlandırıldı.");                    
+                    }else if (islem==2)
+                    {
+                    KartTasima(TODO_LIST,INRPOGRESS_LIST,DONE_LIST);                    
+                    }
+            }
+
+
+            
+            }
+            }
     }
-}
 }
 
     
