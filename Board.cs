fa65b54e7eac3156eb94354ba56841f7671bcf46
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-01-19 22:38:42 +0300

    Kart listeleme metodu board sınıfına eklendi.

--- Board.cs
+++ Board.cs
@@ -2,7 +2,7 @@ using System.Collections;
 namespace ToDo
 {
     
-        class Board /* Yapılacak, yapılan ve yapılmış görev kartlarını liste yapısı ile içeren sınıfımız. Ayrıca bu kartları silebilen, 
+        public class Board /* Yapılacak, yapılan ve yapılmış görev kartlarını liste yapısı ile içeren sınıfımız. Ayrıca bu kartları silebilen, 
                     yeni kartlar ekleyip, kartları 3 başlıkta listeleyebilen ve taşıyabilen metotları içeriyor.*/
         {
         
@@ -16,13 +16,43 @@ namespace ToDo
             DONE.Add(new Kart("Mutfak","Yemek yapmak", "Furkan",Kart.Buyukluk.M));
             INPROGRESS.Add(new Kart("Alışveriş","Ekmek almak", "Esat",Kart.Buyukluk.XS));
         }
+        
+        void KartListele(List<Kart> TODO_LIST, List<Kart> INRPOGRESS_LIST, List<Kart> DONE_LIST)
+            {
+        foreach (var item in TODO_LIST)
+        {
+            Console.WriteLine("TODO LINE\n*******************************************");
+            Console.WriteLine($"Başlık:{item.Baslık}");
+            Console.WriteLine($"İçerik:{item.Icerik}");
+            Console.WriteLine($"Atanan Kişi:{item.AtananKisi}");
+            Console.WriteLine($"Büyüklük:{item.buyukluk}");
+        }
+        foreach (var item in INRPOGRESS_LIST)
+        {
+            Console.WriteLine("IN PROGRESS LINE\n*******************************************");
+            Console.WriteLine($"Başlık:{item.Baslık}");
+            Console.WriteLine($"İçerik:{item.Icerik}");
+            Console.WriteLine($"Atanan Kişi:{item.AtananKisi}");
+            Console.WriteLine($"Büyüklük:{item.buyukluk}");
+        }
+        foreach (var item in DONE_LIST)
+        {
+            Console.WriteLine("DONE LIST\n*******************************************");
+            Console.WriteLine($"Başlık:{item.Baslık}");
+            Console.WriteLine($"İçerik:{item.Icerik}");
+            Console.WriteLine($"Atanan Kişi:{item.AtananKisi}");
+            Console.WriteLine($"Büyüklük:{item.buyukluk}");
+        }
+            
+        }
         void KartSilme(List<Kart> list)
         {
         Console.WriteLine();
 
-        }
+            
 
     }
+}
 }
 
     
